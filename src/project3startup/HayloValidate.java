86bc1597269125d8eaf300e9117070f1dbae0300
Slick_Rick <Slick_Rick@b98-wc2-auth-134-71-208-234.wlan.cpp.edu>
2016-05-27 10:11:55 -0700

    validate

--- src/project3startup/HayloValidate.java
+++ src/project3startup/HayloValidate.java
@@ -1,6 +1,28 @@
 package project3startup;
 
+import javax.swing.JOptionPane;
+
 public class HayloValidate {
 
-
+    public static String getValidName(String aName){
+    
+        while(isValidName(aName) == false){
+             aName = JOptionPane.showInputDialog("Please re-enter  name. value " + aName + " is not valid");
+        }
+        
+        return aName;
+    }
+    
+    
+    private static boolean isValidName(String aName){
+        boolean result = true;
+        
+        for(int i=0; i < aName.length(); i++){
+            if (Character.isLetter(aName.charAt(i)) == false){
+                result = false;
+            }
+        }
+        
+        return result;
+    }
 }

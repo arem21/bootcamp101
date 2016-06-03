d6d82ecdb1c8c651ab4409dbc3034089cd307e2c
vansmuseii <vansmuseii@gmail.com>
2016-06-03 10:36:35 -0700

    fixed customer

--- src/project3startup/HayloCustomer.java
+++ src/project3startup/HayloCustomer.java
@@ -9,85 +9,39 @@ public class HayloCustomer {
 	
 	public HayloCustomer(String aFirstName,	String aLastName, 
                 String aPhone,int aNbrVehicles,	int aNbrTanks){
-
+            firstName = aFirstName;
+            lastName = aLastName;
+            phone = aPhone;
+            nbrVehicles = aNbrVehicles;
+            nbrTanks = aNbrTanks;
 	}
 
+	public String getFirstName(){
+		return firstName;
+	}
+	
+	public String getLastName(){
+		return lastName;            
+	}
+	
+	public String getPhone(){
+		return phone;
+	}
+	
+	public int getNbrVehicles(){
+		return nbrVehicles;
+	}
+	
+	public int getNbrTanks(){
+            return nbrTanks;
+        }
+	
 	public String toString(){
 		String result = "-----------------\n";
-		result += "Customer First Name: " + getFirstName() + "\n";
-		result += "Customer Last Name: " + getLastName() + "\n";
-		result += "Customer Phone: " + getPhone();
+		result += "Customer First Name: " + firstName + "\n";
+		result += "Customer Last Name: " + lastName + "\n";
+		result += "Customer Phone: " + phone;
 		
 		return result;
 	}
-
-    /**
-     * @return the firstName
-     */
-    public String getFirstName() {
-        return firstName;
-    }
-
-    /**
-     * @param firstName the firstName to set
-     */
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     * @return the lastName
-     */
-    public String getLastName() {
-        return lastName;
-    }
-
-    /**
-     * @param lastName the lastName to set
-     */
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     * @return the phone
-     */
-    public String getPhone() {
-        return phone;
-    }
-
-    /**
-     * @param phone the phone to set
-     */
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    /**
-     * @return the nbrVehicles
-     */
-    public int getNbrVehicles() {
-        return nbrVehicles;
-    }
-
-    /**
-     * @param nbrVehicles the nbrVehicles to set
-     */
-    public void setNbrVehicles(int nbrVehicles) {
-        this.nbrVehicles = nbrVehicles;
-    }
-
-    /**
-     * @return the nbrTanks
-     */
-    public int getNbrTanks() {
-        return nbrTanks;
-    }
-
-    /**
-     * @param nbrTanks the nbrTanks to set
-     */
-    public void setNbrTanks(int nbrTanks) {
-        this.nbrTanks = nbrTanks;
-    }
 }
